04dec234f969ced2851e1bd6f2b9de1d270e3302
vdprog2019 <48090405+vdprog2019@users.noreply.github.com>
2019-06-03 14:51:43 +0300

    проверка на наличие url

--- new.php
+++ new.php
@@ -2,5 +2,11 @@
 echo "<link rel='stylesheet' href='cssfile.css'>";
 
 $a = $_POST["pic"];
-echo '<img src="'.$a.'" width="233px" height="233px" class="move">';
+if (filter_var($a, FILTER_VALIDATE_URL) === FALSE) {
+    die('Not a valid URL');
+}
+else{
+    echo '<img src="'.$a.'" width="233px" height="233px" class="move">';
+}
+
 
